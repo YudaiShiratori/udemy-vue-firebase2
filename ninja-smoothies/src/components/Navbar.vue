b34208a65a4dda5b28b163052212666002843707
shiratori yudai <toritorikebabu1023x@gmail.com>
2019-02-01 21:59:53 +0900

    introduce Firebase

---
+++ ninja-smoothies/src/components/Navbar.vue
@@ -0,0 +1,50 @@
+<template>
+  <div class="navbar">
+    <nav class="nav-extended indigo darken-2">
+      <div class="nav-content">
+        <router-link to="">
+          <span class="nav-title">Nav smoothies</span>
+        </router-link>
+        <a href="" class="btn-floating btn-large halfway-fab pink">
+          <router-link to="">
+            <i class="material-icon">Add</i>
+          </router-link>
+        </a>
+      </div>
+    </nav>
+  </div>
+</template>
+
+<script>
+import db from '@/firebase/init'
+export default {
+  name: 'Navbar',
+  data() {
+    return {
+
+    }
+  },
+  methods: {
+
+  },
+  created() {
+    //fetch data from firestore
+    db.collection('smoothies').get().
+    then(snapshot => {
+      snapshot.forEach(doc => {
+        // console.log(doc.data(), doc.id)
+        let smoothie = doc.data()
+        smoothie.id = doc.id
+        this.smoothies.push
+      })
+    })
+  }
+}
+</script>
+
+<style>
+.navber nav {
+  padding: 0 10px;
+}
+</style>
+
---
+++ ninja-smoothies/src/firebase/init.js
@@ -0,0 +1,17 @@
+import firebase from 'firebase'
+import firestore from 'firebase/firestore'
+
+// Initialize Firebase
+var config = {
+  apiKey: "AIzaSyAH2BAcVCvf-u20akw9h49twAWABAE4AOE",
+  authDomain: "udemy-ninja-smoothies-80460.firebaseapp.com",
+  databaseURL: "https://udemy-ninja-smoothies-80460.firebaseio.com",
+  projectId: "udemy-ninja-smoothies-80460",
+  storageBucket: "udemy-ninja-smoothies-80460.appspot.com",
+  messagingSenderId: "388164311361"
+};
+
+const firebaseApp = firebase.initializeApp(config);
+
+//export firestore database
+export default firebaseApp.firestore()
